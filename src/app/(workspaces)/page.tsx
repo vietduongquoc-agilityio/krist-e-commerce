6f215efb5368a741239f7009aa41238fa0c89fe1
Viet Duong <viet.duong@asnet.com.vn>
2025-07-18 09:26:16 +0700

    Feat/implement ui for button component

--- src/app/(workspaces)/page.tsx
+++ src/app/(workspaces)/page.tsx
@@ -1,6 +1,4 @@
-import { Text } from '@/components';
 import { Button } from '@/components/commons/Button';
-import { TEXT_SIZE, TEXT_VARIANT } from '@/interfaces';
 
 export default function LandingPage() {
   return (
@@ -14,18 +12,6 @@ export default function LandingPage() {
         Click Me (Ghost)
       </Button>
       <Button className="mb-4 bg-coralRed">Click Me (Light)</Button>
-
-      <Text size={TEXT_SIZE['2XL']} className="mb-4">
-        This is a primary text with size 2XL.
-      </Text>
-
-      <Text
-        size={TEXT_SIZE['2XL']}
-        variant={TEXT_VARIANT.PRIMARY}
-        className="mb-4"
-      >
-        This is a primary text with size 2XL.
-      </Text>
     </div>
   );
 }
--- src/components/commons/index.ts
+++ src/components/commons/index.ts
@@ -1,3 +1,2 @@
 export * from './Input';
 export * from './Button';
-export * from './Text';
