a44193e97b4eb39df93cc9fcf132ac937337eebb
viet.duong <viet.duong@asnet.com.vn>
2025-07-22 16:32:29 +0700

    Revert code in workspage page

--- src/app/(workspaces)/page.tsx
+++ src/app/(workspaces)/page.tsx
@@ -1,22 +1,9 @@
 import { Banner } from '@/components';
 
-import { ProductCard } from '@/components';
-
 export default function LandingPage() {
   return (
     <>
       <Banner />
-      <ProductCard
-        thumbnailUrl="/images/product-img.webp"
-        title="Shiny Dress"
-        brand="Al Karam"
-        rating={3}
-        reviews="4.1k"
-        price={95.5}
-        status="Almost Sold Out"
-        id={''}
-        documentId={''}
-      />
     </>
   );
 }
