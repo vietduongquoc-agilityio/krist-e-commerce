a6efabe3ac9438a0f78709c2fcfb04fc916d7b12
viet.duong <viet.duong@asnet.com.vn>
2025-07-22 09:39:36 +0700

    Refactor footer component and write storybook

--- src/components/share-layout/Footer/index.tsx
+++ src/components/share-layout/Footer/index.tsx
@@ -4,29 +4,61 @@ import { Navbar, NavbarContent } from '@heroui/react';
 import { usePathname } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Text } from '@/components/commons';
 
-const navItems = [
-  { text: 'Home', path: '/' },
-  { text: 'Shop', path: '/shop' },
-  { text: 'Product', path: '/product' },
-  { text: 'Cart', path: '/cart' },
-];
+// Components
+import { Button, Text } from '@/components/commons';
+import Input from '@/components/commons/Input';
+
+// Constants
+import { NAVITEMS } from '@/constants';
 
 export const Footer = () => {
   const pathname = usePathname();
   return (
     <footer className="w-full items-center flex flex-col pt-36">
-      <figure>
-        <Image
-          src="/images/footer.webp"
-          alt="Footer banner"
-          width={1400}
-          height={747}
-          priority
-        />
-      </figure>
+      <div className="flex justify-between gap-5 items-center">
+        <figure>
+          <Image
+            src="/images/footer-img-left.webp"
+            alt="Footer banner"
+            width={360}
+            height={747}
+            sizes="(100vw - 20px) 100vw, 360px"
+          />
+        </figure>
+        <div className="w-[631px] pt-22 flex flex-col items-center text-center">
+          <h3 className="font-secondary text-[46px] text-charcoal mb-5">
+            Subscribe To Our Newsletter
+          </h3>
+          <p className="text-gray mb-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Scelerisque
+            duis ultrices sollicitudin aliquam sem. Scelerisque duis ultrices
+            sollicitudin
+          </p>
+          <Input
+            placeholder="michael@ymail.com"
+            type="email"
+            inputClassName="border-none px-8 py-[28px] text-[22px] cursor-not-allowed shadow-lg mb-8"
+          />
+          <Button
+            variant="solid"
+            className="w-[200px] cursor-not-allowed"
+            disabled
+          >
+            Subscribe
+          </Button>
+        </div>
 
+        <figure>
+          <Image
+            src="/images/footer-img-right.webp"
+            alt="Footer banner"
+            width={357}
+            height={747}
+            sizes="(100vw - 20px) 100vw, 357px"
+          />
+        </figure>
+      </div>
       <div className="w-full border-t border-lightGray mt-12 pt-7">
         <Navbar className="flex items-center w-[1282px] mx-auto">
           <Link href="/" className="text-4xl font-extrabold font-serif">
@@ -34,7 +66,7 @@ export const Footer = () => {
           </Link>
 
           <NavbarContent className="flex items-center justify-between w-[432px]">
-            {navItems.map((item) => {
+            {NAVITEMS.map((item) => {
               const isActive = pathname === item.path;
               return (
                 <Link
