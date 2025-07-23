cc2b9b355ac96983b66af9ddc4320fe16cc3bb86
Viet Duong <viet.duong@asnet.com.vn>
2025-07-23 17:41:25 +0700

    Feat implement model section component

--- src/components/Sections/AboutSection/index.tsx
+++ src/components/Sections/AboutSection/index.tsx
@@ -1,8 +1,34 @@
 // Components
 import { Text } from '@/components/commons';
+import {
+  IconProtection,
+  IconQuantity,
+  IconShipping,
+  IconSupport,
+} from '@/components';
 
-// Constants
-import { ABOUT_ITEMS } from '@/constants';
+export const ABOUT_ITEMS = [
+  {
+    icon: <IconQuantity />,
+    title: 'High Quality',
+    subtitle: 'crafted from top materials',
+  },
+  {
+    icon: <IconProtection />,
+    title: 'Warrany Protection',
+    subtitle: 'Over 2 years',
+  },
+  {
+    icon: <IconShipping />,
+    title: 'Free Shipping',
+    subtitle: 'Order over 150 $',
+  },
+  {
+    icon: <IconSupport />,
+    title: '24 / 7 Support',
+    subtitle: 'Dedicated support',
+  },
+];
 
 export const AboutSection = () => {
   return (
