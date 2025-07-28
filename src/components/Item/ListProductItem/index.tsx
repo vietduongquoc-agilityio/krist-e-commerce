3275efee5e1368088ef867715a2883dc726ea41f
viet.duong <viet.duong@asnet.com.vn>
2025-07-28 11:02:14 +0700

    feat: implement pagination component and use for ListProductItem

--- src/components/Item/ListProductItem/index.tsx
+++ src/components/Item/ListProductItem/index.tsx
@@ -1,14 +1,19 @@
+'use client';
+
 // Models
 import { ItemCardProps } from '@/models';
 
 // Components
 import { ProductItem } from '@/components/Item';
+import { Pagination } from '@/components';
+import { useState } from 'react';
 
 type ListItemProps = {
   items: ItemCardProps[];
 };
 
 export const ListProductItem = ({ items }: ListItemProps) => {
+  const [currentPage, setCurrentPage] = useState(1);
   if (!items || items.length === 0) {
     return (
       <div className="text-center text-red text-xl py-10">No items found.</div>
@@ -16,10 +21,17 @@ export const ListProductItem = ({ items }: ListItemProps) => {
   }
 
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14">
-      {items.slice(0, 9).map((item) => (
-        <ProductItem key={item.id} {...item} />
-      ))}
+    <section className="items-center flex flex-col gap-16">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-14">
+        {items.map((item) => (
+          <ProductItem key={item.id} {...item} />
+        ))}
+      </div>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={10}
+        onPageChange={setCurrentPage}
+      />
     </section>
   );
 };
