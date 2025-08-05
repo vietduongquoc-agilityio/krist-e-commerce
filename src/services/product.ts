5d52586465f6a04a1a7147a16866c789c8126b5b
viet.duong <viet.duong@asnet.com.vn>
2025-08-05 16:13:10 +0700

    fix: remove console.log

--- src/services/product.ts
+++ src/services/product.ts
@@ -10,16 +10,12 @@ import { apiClient } from '@/services';
 export const getProducts = async () => {
   const url = `${API_ENDPOINTS.PRODUCTS}`;
 
-  console.log('url', url);
-
   const { data, error } = await apiClient.get<{
     data: ProductModel[];
     meta: any;
   }>(url, {
     next: { revalidate: 3600 },
   });
-
-  console.log('dataServices', data);
 
   return { productsData: data?.data, error };
 };
